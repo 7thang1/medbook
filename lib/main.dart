413e04257e4b6e23501813f77ee101f7fed8d053
Thinh-Tan Thi <thinhprocf5879@gmail.com>
2023-05-13 10:55:37 +0700

    remove firebase_core

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
 }
 
