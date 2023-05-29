7959cc587f30646bc9d7a8babe6f9b01d5489db1
EnujV <benzdoll147@gmail.com>
2023-05-29 17:01:13 +0700

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,208 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables, deprecated_member_use, must_be_immutable
+
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:auth_buttons/auth_buttons.dart';
+import 'package:medbook/signup.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  bool showpass = false;
+  TextEditingController usercontroller = new TextEditingController();
+  TextEditingController passcontroller = new TextEditingController();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+          resizeToAvoidBottomInset: false,
+          body: Container(
+            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+            constraints: BoxConstraints.expand(),
+            color: Color(0xFF121a25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                  child: Image.asset(
+                    'assets/LOG2-removebg.png',
+                    width: 100,
+                    height: 100,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Text(
+                    "Welcome",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 40),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 40),
+                  child: Text(
+                    "Sign in your account",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF525761),
+                        fontSize: 25),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: TextField(
+                    controller: usercontroller,
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                    decoration: InputDecoration(
+                      prefixIcon:
+                          Image.asset('assets/mail-icon.png', scale: 20),
+                      fillColor: Color(0xFF202c3c),
+                      filled: true,
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF202c3c)),
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Stack(
+                    alignment: AlignmentDirectional.centerEnd,
+                    children: <Widget>[
+                      TextField(
+                        controller: passcontroller,
+                        obscureText: !showpass,
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                        decoration: InputDecoration(
+                          prefixIcon: Image.asset('assets/password-icon.png',
+                              scale: 20),
+                          fillColor: Color(0xFF202c3c),
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFF202c3c)),
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+                      ),
+                      Padding(
+                          padding: const EdgeInsets.only(right: 10),
+                          child: GestureDetector(
+                            onTap: onToggleShowPass,
+                            child: showpass
+                                ? Image.asset('assets/show.png', scale: 20)
+                                : Image.asset('assets/hide.png', scale: 20),
+                          ))
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 40),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF0f7986),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            )),
+                        onPressed: onSignInPressed,
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        )),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Center(
+                    child: TextButton(
+                      onPressed: onForgotPasswordPressed,
+                      child: Text(
+                        'Forgot your password?',
+                        style:
+                            TextStyle(color: Color(0xFFb9bbbf), fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Center(
+                      child: Text(
+                        'Or Continue With',
+                        style:
+                            TextStyle(color: Color(0xFF525761), fontSize: 20),
+                      ),
+                    )),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 40),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GoogleAuthButton(
+                        onPressed: onGooglePressed,
+                        text: 'Google',
+                        themeMode: ThemeMode.dark,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      FacebookAuthButton(
+                        onPressed: onFacebookPressed,
+                        text: 'Facebook',
+                        themeMode: ThemeMode.dark,
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyle(color: Color(0xFF525761), fontSize: 20),
+                    ),
+                    TextButton(
+                        onPressed: onSignUpPressed,
+                        child: Text(
+                          'Sign up',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ))
+                  ],
+                )
+              ],
+            ),
+          )),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void onToggleShowPass() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      showpass = !showpass;
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  void onSignInPressed() {}
+  void onForgotPasswordPressed() {}
+  void onGooglePressed() {}
+  void onFacebookPressed() {}
+  void onSignUpPressed() {
+    Navigator.push(
+      context as BuildContext,
+      MaterialPageRoute(builder: (context) => SignupScreen()),
     );
   }
 }
---
+++ lib/signup.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:auth_buttons/auth_buttons.dart';
+
+class SignupScreen extends StatefulWidget {
+  @override
+  _SignupState createState() => _SignupState();
+}
+
+class _SignupState extends State<SignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
